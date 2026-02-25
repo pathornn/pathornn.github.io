88b673c8043544f16f074c37b46254a43deb03ba
pathornn <pathornnancha03@gmail.com>
2026-02-25 20:30:42 +0700

    add map and basic front-end

---
+++ kaprao-map/src/components/GoogleMap.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import '../App.css'
+
+function BaseMap() {
+  return (
+    <div className='bg-amber-600'>
+      google Map
+    </div>
+  )
+}
+
+export default BaseMap
